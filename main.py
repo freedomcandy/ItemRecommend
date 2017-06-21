e17f5e479368b1fc7dc75a919a90785a7253e3a7
SunnySWY <apple0105@126.com>
2017-06-21 16:26:09 +0800

    热更新逻辑

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import tornado.ioloop
 import tornado.web
 
 if __name__ == "__main__":
-    application = tornado.web.Application()
+    application = tornado.web.Application(autoreload=True)
     application.listen(8888)
     tornado.ioloop.IOLoop.current().start()

1e4e8d4e01ac0e8688e71380c2197e8d5a55b193
SunnySWY <apple0105@126.com>
2017-06-22 10:56:53 +0800

    传代码自己测一下好吧

--- ml/support_vector_classifier.py
+++ ml/support_vector_classifier.py
@@ -6,7 +6,7 @@ class SupportVectorClassifier:
     # to do handle data
     def processSVMClassifier(self,features_train,labels_train,feature_test):
         features_train = np.array(features_train) 
-        clf = svm.SVC()
+        clf = SVC()
         clf.fit(features_train,labels_train)
         predict = clf.predict(feature_test)
         return predict

6b19785a63caa730562f630cd299ea743c3e0a15
MG001 <MG001@172.16.12.50>
2017-06-22 12:58:48 +0800

    1.MLP return predict categroy id (just only one third categoryId) 2.so fucking hard to type conversion by python

--- ml/multi_percept.py
+++ ml/multi_percept.py
@@ -6,13 +6,16 @@ class MultiLayerPerceptron:
     # define and use Multi-Layer Perceptron to predict the user next tap third-categoryId(serisId) 
     # that return backend maybe two more predicted ID list
     def processMLPClassifier(self, features, labels, predictFeatures):
-        print(type(features))
-        features = np.array(features)
-        thirdCategoryId = []
+        x_data = []
+        for i in features:
+            tempX = [i]
+            x_data.append(tempX)
+       
+        features = np.array(x_data) 
+#         thirdCategoryId = []
         mlp = MLPClassifier()
-        for _ in range(4):
-            mlp.fit(features, labels)
-            y_predict = mlp.predict(predictFeatures)
-            
-        return thirdCategoryId
+        mlp.fit(features, labels)
+        y_predict = mlp.predict(predictFeatures)
+        y_predict.tolist()
+        return y_predict.tolist()
         

a3cf3e972be5c834fd3073ce21e4ae5ea46e8e58
SunnySWY <apple0105@126.com>
2017-06-22 09:46:41 +0800

    连接测试

--- lib/db_con.py
+++ lib/db_con.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 from setting import sql_conf
 from tornado_mysql import pools
 
 pools.DEBUG = True
 
+'''实例化一个'''
 MySQL = pools.Pool(sql_conf)
 
+if __name__ == '__main__':
+    from tornado import ioloop
+    ioloop.IOLoop.current()
+    print(MySQL._opened_conns)

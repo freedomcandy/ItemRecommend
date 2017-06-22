891dd8f9c2968e9f3fa4b0c31fa696c4b3c0a066
SunnySWY <apple0105@126.com>
2017-06-22 10:55:09 +0800

    模拟客户端

---
+++ test_client/main.py
@@ -0,0 +1,19 @@
+import urllib
+from tornado import httpclient, ioloop
+
+def http_test():
+    test_client = httpclient.HTTPClient()
+    try:
+        response = test_client.fetch('http://127.0.0.1:8888/itemRec',
+                                     method = 'POST',
+                                     body = urllib.parse.urlencode(\
+                                                {'tapID': [1,2,3,4,5],
+                                                'tapedID': [2,3,4,5,6]}))
+    except Exception as e:
+        raise e
+    print(response.body)
+    
+if __name__ == '__main__':
+    io_loop = ioloop.IOLoop.current()
+    io_loop.run_sync(http_test)
+        

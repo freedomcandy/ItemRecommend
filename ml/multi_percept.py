8c4491361c3ba75d0e38107ad3e35ad234f1ef98
RoyGuo <253583536@qq.com>
2017-06-26 09:50:01 +0800

    1. MLP added model save and dump

--- ml/multi_percept.py
+++ ml/multi_percept.py
@@ -1,24 +1,49 @@
 # -*- coding: utf-8 -*-
 from sklearn.neural_network import MLPClassifier
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.externals import joblib
 import numpy as np
+import os
 
+SAVE_MODEL_PATH = './save_model'
 
 class MultiLayerPerceptron:
     # define and use Multi-Layer Perceptron to predict the user next tap third-categoryId(serisId) 
     # that return backend maybe two more predicted ID list
-    def processMLPClassifier(self, features, labels, predictFeatures):
+    def processMLPClassifier(self, user_id, features, labels, predictFeatures):
+        #wish data
         x_data = []
         for i in features:
             tempX = [i]
             x_data.append(tempX)
-       
-        features = np.array(x_data) 
-#         thirdCategoryId = []
-        mlp = MLPClassifier(hidden_layer_sizes=(30,20,10))
-        mlp.fit(features, labels)
-        y_predict = mlp.predict(predictFeatures)
+        features = np.array(x_data)
+        
+        #check train model
+        os.chdir(SAVE_MODEL_PATH)
+        mlp = None
+        modelName = '%d_mlp_model.m'%user_id
+        
+        y_predict = None
+        
+        #process data with mlp
+        if os.path.exists(modelName):
+           mlp = joblib.load(modelName)
+           mlp.fit(features, labels)
+           y_predict = mlp.predict(predictFeatures)
+        else:
+            mlp = MLPClassifier(hidden_layer_sizes=(30,20,10))
+            mlp.fit(features, labels)
+            y_predict = mlp.predict(predictFeatures)
+        
+        
+        joblib.dump(mlp,modelName)
         return y_predict.tolist()
+           
+        
+        
+         
+#        
+       
     
     
     

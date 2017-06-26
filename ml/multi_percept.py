544a5adcc4dce871b19f12e5363d6c873d3d546c
RoyGuo <253583536@qq.com>
2017-06-26 09:33:35 +0800

    1.backup

--- ml/multi_percept.py
+++ ml/multi_percept.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from sklearn.neural_network import MLPClassifier
+from sklearn.cluster import AgglomerativeClustering
 import numpy as np
+
 
 class MultiLayerPerceptron:
     # define and use Multi-Layer Perceptron to predict the user next tap third-categoryId(serisId) 
@@ -16,6 +18,27 @@ class MultiLayerPerceptron:
         mlp = MLPClassifier(hidden_layer_sizes=(30,20,10))
         mlp.fit(features, labels)
         y_predict = mlp.predict(predictFeatures)
-        print(y_predict.tolist())
         return y_predict.tolist()
+    
+    
+    
+    def getTargetItemCluster(self,features=None):
+        ac = AgglomerativeClustering(n_clusters= 3)
+        featrue = [[1,2],[1,3],[1,1],[2,1],[2,0],[2,2],[3,3],[2,1],[3,2],[3,1],[1,5]]
+        y_predict = ac.fit_predict(featrue)
+        itemCluster = y_predict[0]
+        cluster_index = []
+        index = 0
+        for i in y_predict:
+            if i == itemCluster:
+                cluster_index.append(index)
+            index+=1  
+#         print(itemCluster)
+#         print(y_predict)
+#         print(cluster_index)
+        return cluster_index
+        
+
+if __name__ == '__main__':
+    MultiLayerPerceptron().getTargetItemCluster()
         

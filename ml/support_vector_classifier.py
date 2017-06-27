e22a6d7932810df62c5051a3446a25277b4393d6
zolobdz <438448706@qq.com>
2017-06-27 09:47:40 +0800

    zolo-add svc handle

--- ml/support_vector_classifier.py
+++ ml/support_vector_classifier.py
@@ -1,17 +1,64 @@
 # -*- coding: utf-8 -*-
 from sklearn.svm import SVC
-import numpy as np
+from sklearn.externals import joblib
+import os
+# from matplotlib.pyplot import clf
+
+SVM_SAVE_MODEL_PATH = '../svm_save_model'
+
 class SupportVectorClassifier:
     #use support vector machine's Support Vector Classifier to resolve classification question
     # to do handle data
-    def processSVMClassifier(self,features_train,labels_train,feature_test):
+    def processSVMClassifier(self,user_id,datas):
         
-        feature_data = []
-        for data in features_train:
-            feature_data.append([data])
+        #handle data 
+        select_feature = ['feature']
+        features = datas[select_feature]
+        print("------",features)
+        labels = datas['label']
+        print("======",labels)
+        predictFeatures = labels[len(labels)-1]
+        print("+++++",predictFeatures)
         
-        features_train = np.array(feature_data) 
+        #calculator with svc
         clf = SVC()
-        clf.fit(features_train,labels_train)
-        predict = clf.predict(feature_test)
-        return predict.tolist()
+        clf.fit(features, labels)
+        y_predict = clf.predict(predictFeatures)
+        
+        #transform
+        y = y_predict.tolist()[0]
+        print('111-----',y)
+        return y
+    
+    def processSVMClassifierLoadSve(self,user_id,datas):
+        #handle data 
+        select_feature = ['feature']
+        features = datas[select_feature]
+        print("------",features)
+        labels = datas['label']
+        print("======",labels)
+        predictFeatures = labels[len(labels)-1]
+        print("+++++",predictFeatures)
+        
+        #check train model
+        os.chdir(SVM_SAVE_MODEL_PATH)
+        clf = None
+        modelName = '%d_svm_model.m'%user_id
+        
+        y_predict = None
+        
+        #Load
+        #process data with SVC
+        if os.path.exists(modelName):
+            clf = joblib.load(modelName)
+            clf.fit(features, labels)
+            y_predict = clf.predict(predictFeatures)
+        else:
+            clf = SVC()
+            clf.fit(features, labels)
+            y_predict = clf.predict(predictFeatures)
+        
+        joblib.dump(clf,modelName)
+        y = y_predict.tolist()[0]
+        print('111-----',y)
+        return y
